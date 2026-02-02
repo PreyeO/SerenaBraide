711714d9b02a4fca26db70945f60d7d902cc870f
PreyeO <omusukup@yahoo.com>
2026-02-02 15:06:00 +0100

    fix deployment bug

--- app/admin/auth/forgot-password/page.tsx
+++
@@ -1,6 +0,0 @@
-import ForgotPasswordForm from "@/features/auth/components/forms/ForgetPasswordForm";
-
-const ForgotPasswordPage = () => {
-  return <ForgotPasswordForm />;
-};
-export default ForgotPasswordPage;
