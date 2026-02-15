6c2e5949b8f859fe26762d28bc86f09b00738dea
PreyeO <omusukup@yahoo.com>
2026-02-15 21:29:28 +0100

    update

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
           />
           <div className="flex items-center px-6 lg:px-12 max-w-122 mt-8.5 border border-white rounded-full">
             <Input
-              className="flex-1 py-5.5 rounded-full border-0 text-sm text-[#F5F5F5] font-normal bg-transparent lg:px-4 px-2"
+              className="flex-1 py-5.5 rounded-full border-0 text-sm text-[#F5F5F5] font-normal bg-transparent lg:px-2"
               placeholder="enter your email address"
             />
             <Button className="bg-white rounded-full size-10 shadow-[0px_16px_40px_-12px_#12121226] flex items-center justify-center">
@@ -43,7 +43,7 @@ const Footer = () => {
           <div className="col-span-2 md:col-span-1 flex flex-col  max-w-47.25">
             <Logo width={134.48} height={50} />
             <Paragraph
-              content="Clean, conscious fragrances that truly leave an impression."
+              content="Scent and beauty for the individual. Designed to be remembered."
               className="text-[#6F6E6C] font-normal text-[10px] leading-3.5 mt-4"
             />
           </div>
@@ -58,7 +58,7 @@ const Footer = () => {
               <ul
                 className={`mt-4 ${
                   section.heading === "WE ACCEPT"
-                    ? "flex items-center lg:flex-wrap  gap-4 max-w-37.75 "
+                    ? "flex items-center flex-wrap  gap-4 max-w-37.75 "
                     : "space-y-4"
                 }`}
               >
